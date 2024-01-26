2afe14b14fa4cce01c070a7984a44eb1ae97d90c
gopal chobey <100458189+gopalchobey9@users.noreply.github.com>
2024-01-26 13:20:41 +0530

    fixed revalidation

--- src/app/page/page.tsx
+++ src/app/page/page.tsx
@@ -1,19 +1,22 @@
-"use client"
+// "use client"
 import CacheClear from "@/components/cache-clear";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  const [ date , setDate] = useState(new Date().getSeconds());
+  // const [ date , setDate] = useState(new Date().getSeconds());
+  const date = new Date().getSeconds();
   return (
     <main className="text-9xl w-screen h-screen flex flex-col items-center justify-center gap-y-10">
       <div>{date}</div>
       
       <div>
-        <Link className="underline" href="/other-page">
+        {/* <Link className="underline" href="/other-page">
           Other Page
-        </Link>
+        </Link> */}
+        {/*  added a tag isntead of Link */}
+        <a href="/other-page">Other Page</a>
       </div>
       <div>
         <CacheClear />
