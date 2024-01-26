e9aed60d132666089613c90234924d9c4268585a
gopal chobey <100458189+gopalchobey9@users.noreply.github.com>
2024-01-26 13:07:23 +0530

    fixes big

--- src/app/page/page.tsx
+++ src/app/page/page.tsx
@@ -1,12 +1,15 @@
+"use client"
 import CacheClear from "@/components/cache-clear";
 import Link from "next/link";
-
+import { useEffect, useState } from "react";
 export const dynamic = "force-dynamic";
 
 export default function Home() {
+  const [ date , setDate] = useState(new Date().getSeconds());
   return (
     <main className="text-9xl w-screen h-screen flex flex-col items-center justify-center gap-y-10">
-      <div>{new Date().getSeconds()}</div>
+      <div>{date}</div>
+      
       <div>
         <Link className="underline" href="/other-page">
           Other Page
