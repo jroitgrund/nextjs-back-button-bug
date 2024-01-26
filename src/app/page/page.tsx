e426fcf05306de373b9703e6a17b1668c6a9930a
gopal chobey <100458189+gopalchobey9@users.noreply.github.com>
2024-01-26 13:30:55 +0530

    fixed revalidation using a tag #61184

--- src/app/page/page.tsx
+++ src/app/page/page.tsx
@@ -1,11 +1,10 @@
-// "use client"
+
 import CacheClear from "@/components/cache-clear";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  // const [ date , setDate] = useState(new Date().getSeconds());
   const date = new Date().getSeconds();
   return (
     <main className="text-9xl w-screen h-screen flex flex-col items-center justify-center gap-y-10">
@@ -15,7 +14,6 @@ export default function Home() {
         {/* <Link className="underline" href="/other-page">
           Other Page
         </Link> */}
-        {/*  added a tag isntead of Link */}
         <a href="/other-page">Other Page</a>
       </div>
       <div>
